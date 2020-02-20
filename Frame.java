2cef1c5d175bb97bb4ab189ba88955d0dee40c73
noise404 <35551060+noise404@users.noreply.github.com>
2020-02-19 19:37:47 -0500

    Add files via upload

---
+++ Frame.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Frame {
+    JFrame base = new JFrame("Calculator" );
+    JPanel buttons = new JPanel();
+    JTextField txtFld = new JTextField();
+
+
+    public Frame(){
+        buttons.setLayout(new GridLayout(4, 4));
+        base.add(buttons);
+        base.add(txtFld);
+        base.setVisible(true);
+
+    }
+
+
+
+}
