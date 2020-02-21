6e27096019292da80a26b23a9b35d41d5f8e0960
Erfboom <vox.universus@gmail.com>
2020-02-21 02:11:06 -0500

    added for loop to add buttons, cleaned up redundancy.

--- CalculatorWindow.java
+++ CalculatorWindow.java
@@ -11,14 +11,21 @@ public class CalculatorWindow extends JFrame{
 		super.setSize(300,300);
 		super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+		JButton[] buttons = new JButton[16];
+		String [] symbols = {"7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", "C", "+", "="};
 		JPanel mainPanel = new JPanel();
 		JPanel topPanel = new JPanel();
 		JPanel bottomPanel = new JPanel();
-		topPanel.setLayout(new GridLayout(1,1));
 		bottomPanel.setLayout(new GridLayout(4,4));
 
 		JTextField textField = new JTextField("0", 20);
 		textField.setEditable(false);
+
+		for (int i = 0; i < buttons.length; i++){
+			buttons[i] = new JButton(symbols[i]);
+			buttons[i].setActionCommand(symbols[i]);
+			bottomPanel.add(buttons[i]);
+		}
 
 		topPanel.add(textField);
 		mainPanel.add(topPanel);
