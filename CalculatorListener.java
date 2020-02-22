5029de988b1025d76edf20c9b9dc9b51dab2ab95
Erfboom <vox.universus@gmail.com>
2020-02-22 03:00:38 -0500

    listener class

---
+++ CalculatorListener.java
@@ -0,0 +1,17 @@
+package Calculator;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CalculatorListener implements ActionListener {
+
+    public void actionPerformed(ActionEvent e){
+
+        //CalculatorWindow.setTextField(e.getActionCommand());
+        CalculatorWindow.setTemp(e.getActionCommand());
+
+    }
+
+
+
+}
